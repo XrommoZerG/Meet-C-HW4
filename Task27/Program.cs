0bc717813393a369446949c4213297a77b197e00
Artem <FxD1ag767@outlook.com>
2023-06-21 13:49:08 +0300

    Task 27 komplit

---
+++ Task27/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Vod(string masseg)
+{
+
+    Console.WriteLine(masseg);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int SumC(int a)
+{
+
+    int result = 0;
+
+    while (a > 0)
+    {
+        result = result + (a % 10);
+        a /= 10;
+    }
+
+    return result;
+}
+
+
+int number = Vod("Введите цифру");
+if (number>0)
+{
+    Console.WriteLine("Сума цифр = " + SumC(number));
+
+}
+else
+{
+        Console.WriteLine("Вы ввели цифру меньше 1");
+
+}
+
+
+
