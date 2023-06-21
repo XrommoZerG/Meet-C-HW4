263185981b4be9b00f4172fd6f98b4ba7def0fa9
Artem <FxD1ag767@outlook.com>
2023-06-21 13:32:48 +0300

    Task 25 komplit

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. НЕ ИСПОЛЬЗОВАТЬ MATH.POW()
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Vod(string masseg){
+
+    Console.WriteLine(masseg);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+
+int number = Vod("Введите число");
+int step = Vod("Введите степень в которую хотите возвести");
+
+ Console.WriteLine("Ответ: число " + number+ " в степени " + step + " равно - " + Math.Pow(number, step));
+
