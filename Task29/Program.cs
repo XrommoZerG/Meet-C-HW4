3d2513dd37921603ed04e67305c7d95cde5ee719
Artem <FxD1ag767@outlook.com>
2023-06-21 14:05:59 +0300

    Task 29 upgrade

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -10,13 +10,44 @@ int Vod(string masseg)
     return a;
 }
 
+int Randoms()
+{
+    int a = Random.Shared.Next(100);
+
+    return a;
+}
+
 
 int[] mass = new int[8];
 
-for (int i = 0; i < mass.Length; i++)
+
+Console.WriteLine("Выберите способ заполнения масива 1-Вручную 2-автомотически от 0 до 99");
+int swit = Convert.ToInt32(Console.ReadLine());
+
+switch (swit)
 {
-    mass[i] = Vod("Введите цифру -" + i);
-    
+    case 1:
+        for (int i = 0; i < mass.Length; i++)
+        {
+            mass[i] = Vod("Введите цифру -" + i);
+
+        }
+
+        break;
+
+    case 2:
+
+        for (int i = 0; i < mass.Length; i++)
+        {
+            mass[i] = Randoms();
+        }
+
+        break;
+
+    default:
+        Console.WriteLine("Вы вели не допустимый вариант");
+
+        break;
 }
 
 Console.WriteLine("Ввывод масива ");
