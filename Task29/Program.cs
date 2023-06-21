744db78f7eebd4cd6b9f6e6124b07de500fe0c2d
Artem <FxD1ag767@outlook.com>
2023-06-21 13:57:52 +0300

    Task 29 komplit

---
+++ Task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+int Vod(string masseg)
+{
+
+    Console.WriteLine(masseg);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+
+int[] mass = new int[8];
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = Vod("Введите цифру -" + i);
+    
+}
+
+Console.WriteLine("Ввывод масива ");
+for (int i = 0; i < mass.Length; i++)
+{
+    Console.WriteLine(mass[i]);
+}
+
